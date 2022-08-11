70137133150324835b94014ebceca3d5a27eddf2
Vinod <vkv.vinod@gmail.com>
2022-08-11 11:19:41 +0400

    petpage

--- cypress/e2e/pet-clinic/pet-page.cy.js
+++ cypress/e2e/pet-clinic/pet-page.cy.js
@@ -27,11 +27,23 @@ var values =[];
     })
 
     it("should add pets ",() => {
+        var dDate= "2022-06-20"
         cy.get(".form-group").first().find("label").first().invoke("text").should("contain","Owner");
         cy.get(".form-group").first().find("div").first().invoke("text").should("include",values[0]+" "+values[1])
         cy.get('#name').type("Tomy");
-        //cy.get("")
+        cy.get("#birthDate").invoke("val",dDate);
+        cy.get("#type").select("dog");
+        cy.get(".btn").should("be.visible").click();
+        cy.get(".dl-horizontal").find('dd').should("have.length",3).each(($el,$index)=>{
+            if($index == 0){
+                expect($el.text()).to.eq("Tomy")
+            }else if($index == 1){
+                expect($el.text()).to.eq(dDate)
 
+            }else{
+                expect($el.text()).to.eq("dog")
+            }
+        });
     })
 
 
